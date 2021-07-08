d159ebcfedf8324d5638dfaddf6a5b9b533f5d96
s1280171 <s1280171@u-aizu.ac.jp>
2021-07-08 23:23:10 +0900

    Greetings added

--- Dice_Game.py
+++ Dice_Game.py
@@ -3,6 +3,13 @@ import random
 print("Rolling the dice...")
 num1=random.randint(1,6)
 num2=random.randint(1,6)
+num3=num1+num2
 print("Die 1: ",num1)
 print("Die 2: ",num2)
 print("Total value:",num1+num2)
+
+if num3>7:
+    print("You won")
+
+else:
+    print("You lost")
